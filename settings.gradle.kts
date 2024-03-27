fc40a887a2bfd7ed01e65f573b1de0d6b250aa6b
Gerben Jongerius <g.jongerius@jong-soft.com>
2024-03-27 16:32:44 +0100

    Update the build pipeline for automated deployment and building of Pledger.io.

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+rootProject.name = "pledger-io"
+
+pluginManagement {
+    plugins {
+        id("java")
+        id("io.micronaut.application").version("4.3.4")
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage") // It's gradle, any of their APIs can be considered unstable
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/pledger-io/build-tooling")
+            credentials {
+                username = providers.gradleProperty("gpr.user").get() ?: System.getenv("GITHUB_ACTOR")
+                password = providers.gradleProperty("gpr.key").get() ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    versionCatalogs {
+        create("pledger") {
+            val apiVersion: String by settings
+            val uxVersion: String by settings
+
+            library("api", "com.jongsoft.finance", "fintrack-api").version(apiVersion)
+            library("ux", "com.jongsoft.finance", "pledger-ui").version(uxVersion)
+        }
+    }
+}
